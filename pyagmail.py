cbbfba4d85e1bba440437fb169fee1b976f4fdcc
Antonio Trento <antoniotrento83@gmail.com>
2020-03-11 21:04:55 +0100

    Update pyagmail.py

--- pyagmail.py
+++ pyagmail.py
@@ -11,7 +11,7 @@ A great day
 
 Thanks"""
 yag = yagmail.SMTP('trendcolorsagl@gmail.com', 'Sciroccobianca1983')
-with open('mendrisio-chiasso-immobiliari.csv') as csv_file:
+with open('data-file.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
     for row in csv_reader:        
