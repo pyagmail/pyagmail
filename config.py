b8a7270e1ae4a6fb45811b1cbb57d69f0c05d1ee
Antonio Trento <antoniotrento83@gmail.com>
2020-03-11 22:34:38 +0100

    Update and rename config.json to config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+import preprocessing
+email = {'subject': 'The subject of the email',
+        'body': 'root',
+        'email': 'senderemail@gmail.com',
+        'passwd': 'my-secret-password',
+        'timer': 100,
+        'file': 'filename.csv'}
+    
