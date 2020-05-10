42b04bedff24924c6f869788621676782c66d320
Antonio Trento <antoniotrento83@gmail.com>
2020-05-10 15:56:46 +0200

    Delete old-config.py

--- old-config.py
+++
@@ -1,7 +0,0 @@
-email = {'subject': 'The subject of the email',
-        'body': 'root',
-        'email': 'senderemail@gmail.com',
-        'passwd': 'my-secret-password',
-        'timer': 100,
-        'file': 'filename.csv'}
-    
