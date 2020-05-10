e25adafb3a37058c7539bd4212510b8c1f9e4e68
Antonio Trento <antoniotrento83@gmail.com>
2020-05-10 15:54:12 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-email = {'subject': 'inviare con yagmail',
-        'body': 'Testo della email',
-        'email': 'contatti.trendcolor@gmail.com',
-        'passwd': 'Sciroccobianca1983',
+email = {'subject': 'Put here the email subject',
+        'body': 'filename-of-the-body.txt',
+        'email': 'sender.email@gmail.com',
+        'passwd': 'SenderPassword',
         'timer': 100,
         'file': 'email-list.csv'}
